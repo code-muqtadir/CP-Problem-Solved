e2fc8f1fe66cc7fc5c4161eca45aadc80085c235
code-muqtadir <arthosowdagor@gmail.com>
2026-09-15 00:07:38 +0600

    Solve Magnets

---
+++ Codeforces/344A/Magnets.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int count=1;
+    string arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]==arr[i-1])
+        {
+            continue;
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
